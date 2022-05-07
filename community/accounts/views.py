87311694c7c721980e59f193544a8ae976800e0e
LBH <hitlbh777@gmail.com>
2022-05-07 15:52:01 +0900

    0507community-delete

--- community/accounts/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import get_user_model
-
-
-# Create your views here.
--- community/mainapps/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainappsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mainapps'
