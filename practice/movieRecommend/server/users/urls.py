343108388b372ad80f88fb88fbde4a4267cbf5a4
LBH <hitlbh777@gmail.com>
2022-07-10 18:36:10 +0900

    reform document structure

--- practice/movieRecommend/server/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('profile/<username>/',views.user_profile_or_follow),
-    path('<int:movie_id>/',views.read_or_create_rating),
-    path('<int:movie_id>/<int:rating_pk>/',views.update_or_delete_rating),
-    path('<int:movie_id>/<int:rating_pk>/like',views.like_rating),
-]
